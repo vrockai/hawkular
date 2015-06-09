321146696c1127b61fc94d0706c6fb22898eacb6
Viliam Rockai <vrockai@redhat.com>
2015-06-09 17:17:34 +0200

    HAWKULAR-152: Metrics 0.3.5 related console update

--- ui/console/src/main/scripts/plugins/metrics/plugins/metrics/ts/metricsResponsePage.ts
+++ ui/console/src/main/scripts/plugins/metrics/plugins/metrics/ts/metricsResponsePage.ts
@@ -196,8 +196,7 @@ module HawkularMetrics {
       }
 
       if (metricId) {
-        this.HawkularMetric.NumericMetricData.queryMetrics({
-          tenantId: this.$rootScope.currentPersona.id,
+        this.HawkularMetric.GaugeMetricData(this.$rootScope.currentPersona.id).queryMetrics({
           numericId: metricId,
           start: startTime,
           end: endTime,
@@ -232,8 +231,7 @@ module HawkularMetrics {
 
 
       if (metricId) {
-        this.HawkularMetric.NumericMetricData.queryMetrics({
-          tenantId: this.$rootScope.currentPersona.id,
+        this.HawkularMetric.GaugeMetricData(this.$rootScope.currentPersona.id).queryMetrics({
           numericId: metricId,
           start: startTime,
           end: endTime,
